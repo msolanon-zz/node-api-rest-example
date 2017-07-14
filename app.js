9e5c220c098c03c557fbcd8dcd04fea5a3b6a955
msolanon <msolanon1994@gmail.com>
2017-07-14 10:48:42 -0600

    new routes in app.js

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-var express = require("express"),  
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require('mongoose'); /*Estos son los imports*/
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(methodOverride());
 
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
 
-var router = express.Router(); /*Inicialización del enrutador*/
-
-router.get('/', function(req, res) {  
-   res.send("Hello World!");
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
 });
-
 app.use(router);
 
-app.listen(3000, function() {  
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
 });
-
